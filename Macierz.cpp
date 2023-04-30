f373692b1bdac01e5951351b15638d32e3e84c34
Marcel <marziel2001@gmail.com>
2023-04-30 15:02:59 +0200

    Upload

---
+++ Macierz.cpp
@@ -0,0 +1,197 @@
+#include "Macierz.h"
+#include <iostream>
+
+using std::cout;
+
+double Macierz::residuum = 1e-9;
+
+Macierz::Macierz(int rows, int cols)
+{
+    this->n = rows;
+
+    typ = macierz;
+
+    M = new double* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        this->M[i] = new double[cols];
+    }
+
+    this->InicjalizujMacierz(rows, cols, 0);
+}
+Macierz::Macierz(int rows)
+{
+    typ = wektor;
+    this->n = rows; 
+
+    V = new double[rows];
+
+    this->InicjalizujWektor(rows, 0);
+}
+Macierz::~Macierz()
+{
+    if (typ == macierz)
+    {
+      
+        for (int i = 0; i < this->n; i++)
+        {
+            delete[] this->M[i];
+        }
+
+        delete[] this->M;
+    }
+    else if (typ == wektor)
+    {
+        delete[] this->V;
+    }
+}
+
+void Macierz::InicjalizujMacierz(int rows, int cols, double wartosc)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            this->M[i][j] = wartosc;
+        }
+    }
+}
+
+void Macierz::InicjalizujWektor(int rows, double wartosc)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        this->V[i] = wartosc;
+    }
+}
+
+void Macierz::StworzMacierzeLUD(Macierz*& L, Macierz*& U, Macierz*& D)
+{
+    for (int i = 0; i < this->n; i++)
+    {
+        for (int j = 0; j < this->n; j++)
+        {
+            if (i > j)
+            {
+                L->M[i][j] = this->M[i][j];
+            }
+            else if (i < j)
+            {
+                U->M[i][j] = this->M[i][j];
+            }
+            else
+            {
+                D->M[i][j] = this->M[i][j];
+            }
+        }
+    }
+}
+
+void Macierz::Jacobi(Macierz*& B, Macierz*& X, Macierz*& Wynik)
+{
+    Macierz* L = new Macierz(this->n, this->n);
+    Macierz* U = new Macierz(this->n, this->n);
+    Macierz* D = new Macierz(this->n, this->n);
+
+    this->StworzMacierzeLUD(L, U, D);
+
+    L->Drukuj();
+    U->Drukuj();
+    D->Drukuj();
+
+}
+
+
+
+void Macierz::GaussSeidl()
+{
+    cout << "Hello GS";
+
+}
+
+Macierz*& Macierz::Dodawanie(Macierz*& A, Macierz*& B)
+{
+    Macierz* C = new Macierz(A->n, A->n);
+
+    for (int i = 0; i < A->n; i++)
+    {
+        for (int j = 0; j < A->n; j++)
+        {
+            C->M[i][j] = B->M[i][j] + A->M[i][j];
+        }
+    }
+
+    return C;
+}
+Macierz*& Macierz::Mnozenie(Macierz*& A, Macierz*& B)
+{
+    Macierz* C = new Macierz(A->n, A->n);
+
+    for (int i = 0; i < A->n; i++)
+    {
+        for (int j = 0; j < A->n; j++)
+        {
+            
+        }
+    }
+
+    return C;
+}
+
+
+
+void Macierz::faktoryzacjaLU()
+{
+
+}
+
+
+void Macierz::Drukuj()
+{
+    if (typ == macierz)
+    {
+        cout << "\n[\n ";
+        for (int i = 0; i < this->n; i++)
+        {
+            cout << "\t[";
+            for (int j = 0; j < this->n; j++)
+            {
+                cout << M[i][j] << ", ";
+            }
+            cout << "];\n";
+        }
+        cout << "]\n";
+    }
+    else if (typ == wektor)
+    {
+        cout << "\n[ ";
+        for (int i = 0; i < this->n; i++)
+        {
+            cout << V[i] << ", ";
+            
+        }
+        cout << " ]\n";
+    }
+}
+
+void Macierz::UstawDiagonale(double a1, double a2, double a3, int rozmiar)
+{
+    for (int i = 0; i < rozmiar; i++)
+    {
+        for (int j = 0; j < rozmiar; j++)
+        {
+            if (i == j)
+            {
+                M[i][j] = a1;
+            }
+            if (i == j - 1 || i == j + 1)
+            {
+                M[i][j] = a2;
+            }
+            if (i == j - 2 || i == j + 2)
+            {
+                M[i][j] = a3;
+            }
+        }
+    }
+}
---
+++ Macierz.h
@@ -0,0 +1,42 @@
+#pragma once
+
+class Macierz
+{
+	enum typ
+	{
+		macierz, 
+		wektor
+	};
+
+public:
+
+	double** M = nullptr;
+	double* V = nullptr;
+
+	int n = 0;
+	static double residuum;
+
+	Macierz(int rows, int cols);
+	Macierz(int rows);
+	~Macierz();
+
+	void Drukuj();
+	void UstawDiagonale(double a1, double a2, double a3, int rozmiar);
+	void StworzMacierzeLUD(Macierz*& L, Macierz*& U, Macierz*& D);
+	void InicjalizujMacierz(int rows, int cols, double wartosc);
+	void InicjalizujWektor(int rows, double wartosc);
+
+	// Dzialania
+	static Macierz*& Dodawanie(Macierz*& A, Macierz*& B);
+	static Macierz*& Mnozenie(Macierz*& A, Macierz*& B);
+
+	void Jacobi(Macierz*& B, Macierz*& X, Macierz*& R);
+	void GaussSeidl();
+	void faktoryzacjaLU();
+
+private:
+	typ typ = macierz;
+
+
+
+};
